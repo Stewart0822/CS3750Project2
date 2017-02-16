32763043157a1aa5fad29d3e3009a2cc28f77dc7
Daniel Bigelow <Interfer0@gmail.com>
2017-02-16 16:12:24 -0700

    Removed all remaining chat functionality from App.js

--- routes/chat.js
+++ routes/chat.js
@@ -1,15 +1,32 @@
-var express = require('express');
-var router = express.Router();
+module.exports = function(io) {
 
-router.all(function(request, response, next) {
-    //if not req.session.userID redirect to login page
-    //else invoke next
-    next();
-})
-
-router.get("/chat", function(request, response, next) {
-    response.render("chatroom.jade");
-});
+    var express = require('express');
+    var router = express.Router();
 
 
-module.exports = router;
+    router.all(function(request, response, next) {
+        //if not req.session.userID redirect to login page
+        //else invoke next
+        next();
+    })
+
+    router.get("/chat", function(request, response, next) {
+        response.render("chatroom.jade");
+    });
+
+
+    router.post("/message", function(request, response) {
+        var message = request.body.message;
+        if (request.body.message == undefined || request.body.message.trim() == "") {
+            return response.status(400).json({ error: "Message is invalid" });
+        }
+        var name = request.body.name;
+        io.sockets.emit("incomingMessage", { message: message, name: name });
+
+        response.status(200).json({ message: "Message received" });
+    });
+
+    return router
+}
+
+//module.exports = router;
