d274b6bd3d837502a8ebd473a312ec911d946eed
Daniel Bigelow <Interfer0@gmail.com>
2017-02-18 14:10:22 -0700

    Changed path for javascript require in login.jade, changed login.js to use init to setup listeners.

--- public/js/login.js
+++ public/js/login.js
@@ -1,24 +1,34 @@
 //login.jade//
+function init() {
 
-document.getElementById("btnLogin").onclick = function() {validateLogin();};
-document.getElementById("name").onclick = function() {clearNameErr();};
-document.getElementById("pwd").onclick = function() {clearPwdErr();};
+    function validateLogin() {
+        if (document.getElementById("name").value === "") {
+            document.getElementById("nameErr").innerHTML = "Requiered";
+            nameErr.style = "color:blue";
+        }
+        if (document.getElementById("pwd").value === "") {
+            document.getElementById("pwdErr").innerHTML = "Requiered";
+            pwdErr.style = "color:blue";
+        }
+    }
 
-function validateLogin() {
-  if(document.getElementById("name").value === ""){
-    document.getElementById("nameErr").innerHTML = "Requiered";
-    nameErr.style = "color:blue";
-  }
-  if(document.getElementById("pwd").value === ""){
-    document.getElementById("pwdErr").innerHTML = "Requiered";
-    pwdErr.style = "color:blue";
-  }
+    function clearNameErr() {
+        document.getElementById("nameErr").innerHTML = "";
+        document.getElementById("name").value = "";
+    }
+
+    function clearPwdErr() {
+        document.getElementById("pwdErr").innerHTML = "";
+        document.getElementById("pwd").value = "";
+    }
+
+
+    $('#btnLogin').on('click', validateLogin);
+    $('#name').on('click', clearNameErr);
+    $('#pwd').on('click', clearPwdErr);
+
+
 }
-function clearNameErr(){
-  document.getElementById("nameErr").innerHTML = "";
-  document.getElementById("name").value = "";
-}
-function clearPwdErr(){
-  document.getElementById("pwdErr").innerHTML = "";
-  document.getElementById("pwd").value = "";
-}
+
+
+$(document).on('ready', init);
