5d8e30c0a2b3fa1eed41cac6e0b8592cc7cdad40
Morgan Dahl <morgancdahl@gmail.com>
2017-02-24 08:32:34 -0700

    UserName validation and password concealed.

--- public/js/login.js
+++ public/js/login.js
@@ -32,7 +32,39 @@ function init() {
         });
     }  
 
-    $('#login').on('click', loginNow)
-    $('#logout').on('click', logoutNow)
+    function validateForm(event){
+        var userNameRegex = /^[a-zA-Z0-9]+$/;
+        var userNameInput = $('#username').val();
+        var result = userNameRegex.test(userNameInput);
+        //var validUsername = document.frm.userName.username.match(userNameRegex);
+        if(result == false){
+            alert("Your user name is not valid.");
+            //document.frm.userName.focus();
+        }
+        else {
+            loginNow();
+        }
+    }
+
+    function handleEnterKey(e){
+         if (e.keyCode == 13) {
+            validateForm();
+        }
+    }
+    //$('#login').on('click', validateForm)
+    //if (validateForm() == false) {
+
+    //}
+    //else {
+        //$('#login').on('click', loginNow)
+        //$('#logout').on('click', logoutNow)
+    //}
+     //('#login').on('click', loginNow)
+     $('#login').on('click', loginNow);
+     $('#logout').on('click', logoutNow);
+     $('#login').on('click', validateForm);
+     $("#username").keydown(handleEnterKey);
+     $("#password").keydown(handleEnterKey);
 }
+
 $(document).on('ready', init);
