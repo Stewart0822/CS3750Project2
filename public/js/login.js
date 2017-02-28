9dcf7b165344ba98bfd851d787a0b3944220734c
Stewart0822 <A.Stewart0822@gmail.com>
2017-02-28 00:21:44 -0700

    Email, Username, FirstName, LastName Regex validaton of Login and Register.

--- public/js/login.js
+++ public/js/login.js
@@ -2,46 +2,48 @@
 function init() {
 
     function validateLogin() {
-        if (document.getElementById("name").value === "") {
-            document.getElementById("nameErr").innerHTML = "Requiered";
-            nameErr.style = "color:blue";
-        }
-        if (document.getElementById("pwd").value === "") {
-            document.getElementById("pwdErr").innerHTML = "Requiered";
-            pwdErr.style = "color:blue";
-        }
-    }
+        
+        var userNameRegex = /^[a-zA-Z0-9]+$/;
+        var userNameInput = $('#name').val();
+        var passwordInput = $('#pwd').val();
+        var userresult = userNameRegex.test(userNameInput);
+        var passresult = userNameRegex.test(passwordInput);
 
+        if (document.getElementById("name").value === "" || userresult == false) {
+            if(document.getElementById("name").value === ""){
+                document.getElementById("name").style = "background-color:#99ff99";
+                document.getElementById("name").placeholder = "Requiered";
+            }
+            else{
+               document.getElementById("name").value = "";
+               document.getElementById("name").style = "background-color:#99ff99";
+               document.getElementById("name").placeholder = "Invalid Username";
+            }
+        
+        }
+        if (document.getElementById("pwd").value === "" || emailresult == false) {
+            if(document.getElementById("pwd").value === ""){
+                document.getElementById("pwd").style = "background-color:#99ff99";
+                document.getElementById("pwd").placeholder = "Requiered";
+            }
+        }
+        
+    }
     function clearNameErr() {
-        document.getElementById("nameErr").innerHTML = "";
-        document.getElementById("name").value = "";
+        document.getElementById("name").placeholder = "";
+        document.getElementById('name').style = "background-color:#ffffff";     
     }
-
     function clearPwdErr() {
-        document.getElementById("pwdErr").innerHTML = "";
-        document.getElementById("pwd").value = "";
-    }
-
-    function sendMessage() {
-        var outgoingMessage = $('#outgoingMessage').val();
-        var name = $('#name').val();
-        $.ajax({
-            url: '/message',
-            type: 'POST',
-            contentType: 'application/json',
-            dataType: 'json',
-            data: JSON.stringify({ message: outgoingMessage, name: name })
-        });
+       document.getElementById("pwd").placeholder = "";
+       document.getElementById('pwd').style = "background-color:#ffffff"; 
     }
 
     
-    }
+    
   
     $('#btnLogin').on('click', validateLogin);
-    $('#name').on('click', clearNameErr);
-    $('#pwd').on('click', clearPwdErr);
-
-
+    $('#name').on('focus', clearNameErr);
+    $('#pwd').on('focus', clearPwdErr);
 }
 
 
--- public/js/register.js
+++ public/js/register.js
@@ -1,20 +1,65 @@
 function init(){
-    function validateRegister() {
-        if (document.getElementById("fName").value === "") {
-            document.getElementById("fName").style = "background-color:#99ff99";
-            document.getElementById("fName").placeholder = "Requiered";
+    
+    function validateRegister(event) {
+
+        var firstNameRegex = /^[a-zA-Z]+$/;
+        var lastNameRegex = /^[a-zA-Z]+$/;
+        var userNameRegex = /^[a-zA-Z0-9]+$/;
+        var emailRegex = /^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/;
+        var firstNameInput = $('#fName').val();
+        var lastNameInput = $('lName').val();
+        var userNameInput = $('#uName').val();
+        var passwordInput = $('#pwd').val();
+        var emailInput = $('#email').val();
+        var userresult = userNameRegex.test(userNameInput);
+        var lnameresult = lastNameRegex.test(lastNameInput);
+        var fnameresult = firstNameRegex.test(firstNameInput);
+        var passresult = userNameRegex.test(passwordInput);
+        var emailresult = emailRegex.test(emailInput); 
+        
+        if (document.getElementById("fName").value === "" || fnameresult == false) {
+            if (document.getElementById("fName").value === ""){
+                document.getElementById("fName").style = "background-color:#99ff99";
+                document.getElementById("fName").placeholder = "Requiered";
+            }
+            else{
+               document.getElementById("fName").value = "";
+               document.getElementById("fName").style = "background-color:#99ff99";
+               document.getElementById("fName").placeholder = "Invalid First Name";
+            }
         }
-        if (document.getElementById("lName").value === "") {
-            document.getElementById("lName").style = "background-color:#99ff99";
-            document.getElementById("lName").placeholder = "Requiered";
+        if (document.getElementById("lName").value === "" || lnameresult == false) {
+            if (document.getElementById("lName").value === "") {
+                document.getElementById("lName").style = "background-color:#99ff99";
+                document.getElementById("lName").placeholder = "Requiered";
+            }
+            else{
+               document.getElementById("lName").value = "";
+               document.getElementById("lName").style = "background-color:#99ff99";
+               document.getElementById("lName").placeholder = "Invalid Last Name";
+            }
         }
-        if (document.getElementById("uName").value === "") {
-            document.getElementById("uName").style = "background-color:#99ff99";
-            document.getElementById("uName").placeholder = "Requiered";
+        if (document.getElementById("uName").value === "" || userresult == false) {
+            if(document.getElementById("uName").value === ""){
+                document.getElementById("uName").style = "background-color:#99ff99";
+                document.getElementById("uName").placeholder = "Requiered";
+            }
+            else{
+               document.getElementById("uName").value = "";
+               document.getElementById("uName").style = "background-color:#99ff99";
+               document.getElementById("uName").placeholder = "Invalid Username";
+            }        
         }
-        if (document.getElementById("email").value === "") {
-            document.getElementById("email").style = "background-color:#99ff99";
-            document.getElementById("email").placeholder = "Requiered";
+        if (document.getElementById("email").value === "" || emailresult == false) {
+            if(document.getElementById("email").value === ""){
+                document.getElementById("email").style = "background-color:#99ff99";
+                document.getElementById("email").placeholder = "Requiered";
+            }
+            else{
+                document.getElementById("email").value = "";
+                document.getElementById("email").style = "background-color:#99ff99";
+                document.getElementById("email").placeholder = "Invalid Email Address";
+            }
         }
         if (document.getElementById("pwd").value === "") {
             document.getElementById("pwd").style = "background-color:#99ff99";
@@ -24,95 +69,68 @@ function init(){
             document.getElementById("rPwd").style = "background-color:#99ff99";
             document.getElementById("rPwd").placeholder = "Requiered";
         }
-
     }//end validate user
-
     function clearFnameErr() {
-        document.getElementById("fNameErr").innerHTML = "";        
+        document.getElementById("fName").placeholder = "";
+        document.getElementById('fName').style = "background-color:#ffffff";        
     }
     function clearLnameErr() {
-        document.getElementById("lNameErr").innerHTML = "";        
+        document.getElementById("lName").placeholder = "";
+        document.getElementById('lName').style = "background-color:#ffffff";
     }
     function clearUnameErr() {
-        document.getElementById("uNameErr").innerHTML = "";        
+        document.getElementById("uName").placeholder = "";
+        document.getElementById('uName').style = "background-color:#ffffff";     
     }
     function clearEmailErr() {
-        document.getElementById("emailErr").innerHTML = "";        
+        document.getElementById("email").placeholder = "";
+        document.getElementById('email').style = "background-color:#ffffff";     
     }
     function clearPwdErr() {
-        document.getElementById("pwdErr").innerHTML = "";        
+       document.getElementById("pwd").placeholder = "";
+       document.getElementById('pwd').style = "background-color:#ffffff"; 
     }
     function clearRpwdErr() {
-        document.getElementById("rPwdErr").innerHTML = "";        
+       document.getElementById("rPwd").placeholder = "";
+       document.getElementById('rPwd').style = "background-color:#ffffff";       
     }
-    function loginNow() {
-        var username = $('#username').val();
-        var password = $('#password').val();
-        $.ajax({
-            url: '/Users/login',
-            type: 'POST',
-            contentType: 'application/json',
-            dataType: 'json',
-            data: JSON.stringify({ username: username, password: password }),
-            success: function(data, textStatus, jqXHR) {
-                window.location = '../chat';
-            },
-            error: function(err) { //On Error will need to popup banner that there was an error. from Adam's code
-                console.log(err);
-            }
-
-        });
-    }
-
-    function logoutNow() {
-        $.ajax({
-            url: '/Users/logout',
-            type: 'POST',
-            contentType: 'application/json',
-            dataType: 'json',
-            data: JSON.stringify({}),
-            success: function(data, textStatus, jqXHR) {
-                window.location = '../'
-            }
-        });
-    }  
-
-    function validateForm(event){
-        var userNameRegex = /^[a-zA-Z0-9]+$/;
-        var userNameInput = $('#username').val();
-        var passwordInput = $('#password').val();
-        var userresult = userNameRegex.test(userNameInput);
-        var passresult = userNameRegex.test(passwordInput);
-        //var validUsername = document.frm.userName.username.match(userNameRegex);
-        if(userresult == false && passresult == true){
-            alert("Your user name is not valid.\nOnly include letters or numbers");
-            //document.frm.userName.focus();
+    // function registerNow(){
+    //     var firstname = $('#fName').val();
+    //     var lastname = $('#lName').val(); 
+    //     var password = $('#pwd').val();
+    //     var username = $('#uName').val();
+    //     $.ajax({
+    //         url: '/Users/register',
+    //         type: 'POST',
+    //         contentType: 'application/json',
+    //         dataType: 'json',
+    //         data: JSON.stringify({ fisrtname: fName, lastname: lName, username: username, password: password }),
+    //         success: function(data, textStatus, jqXHR) {
+    //             window.location = '../chat';
+    //         },
+    //         error: function(err) { //On Error will need to popup banner that there was an error. from Adam's code. Need to know error codes :)
+    //             console.log(err);
+    //         }
+    //     });
+    // }
+    function handleEnterKey(event){
+         if (event.keyCode == 13) {
+            validateRegister();
         }
-        else if(userresult == false && passresult == false) {
-            alert("Your user name and password are not valid.\nOnly include letters or numbers");
-        }
-        else if(passresult == false && userresult == true) {
-            alert("Your password is not valid.\nOnly include letters or numbers.");
-        }
-        else {
-            loginNow();
-        }
-
     }
 
-    function handleEnterKey(e){
-         if (e.keyCode == 13) {
-            validateForm();
-        }
-    }
+ 
     $('#btnRegister').on('click', validateRegister);
-    $('#fName').on('click', clearFnameErr);
-    $('#lName').on('click', clearLnameErr);
-    $('#uName').on('click', clearUnameErr);
-    $('#email').on('click', clearEmailErr);
-    $('#pwd').on('click', clearPwdErr);
-    $('#rPwd').on('click', clearRpwdErr);
-
-
+    $('#fName').on('focus', clearFnameErr);
+    $('#lName').on('focus', clearLnameErr);
+    $('#uName').on('focus', clearUnameErr);
+    $('#email').on('focus', clearEmailErr);
+    $('#pwd').on('focus', clearPwdErr);
+    $('#rPwd').on('focus', clearRpwdErr);
+    //-----------------------------------------------------------------------------
+    $('#login').on('click', registerNow);
+    //$('#btnRegister').on('click', validateForm);
+    $('#fName').on('keydown', handleEnterKey);
+    $('#lName').on('keydown',handleEnterKey);
 }
 $(document).on('ready', init);
